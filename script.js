5f856ca1e433505981fc78b642d3565bcea38a51
kaungmyat636 <km6355183@gmail.com>
2026-08-07 23:00:45 +0630

    Remove default birthday message logic

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-// Default values if no URL parameters are provided
-const urlParams = new URLSearchParams(window.location.search);
-if (!urlParams.has('name')) {
-    const nameEl = document.getElementById('name');
-    const msgEl = document.getElementById('message');
-    if (nameEl) nameEl.innerText = "Happy Birthday, Bbe!"; // 
-    if (msgEl) msgEl.innerText = "Wishing you all the best on your special day!";
-}
+
